83a266a10dcc380a29464f43ec61bbadde4fb52d
garatquent <60227666+garatquent@users.noreply.github.com>
2023-03-27 16:18:49 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -26,5 +26,26 @@ async def root():
     return {"message": "Hello World"}
 
 @app.get("/test/")
-async def test(request: Request, name : str, deepness1 : float, deepness2 : float, sandstone: float, clay: float, latitude: str, longitude: str, description: Union[str, None] = None):
-    return templates.TemplateResponse("index.html", {"request": request, "name": name, "deepness1": deepness1, "deepness2": deepness2, "sandstone": sandstone, "clay":clay, "latitude": latitude, "longitude": longitude, "description": description})
+async def test(
+        request: Request,
+        name : str = "Well",
+        deepness1 : float = 0,
+        deepness2 : float = 0,
+        sandstone: float = 0,
+        clay: float = 0,
+        latitude: str = "Not available in this document",
+        longitude: str = "Not available in this document",
+        description: Union[str, None] = None
+    ):
+
+    return templates.TemplateResponse("index.html", {
+            "request": request,
+            "name": name,
+            "deepness1": deepness1,
+            "deepness2": deepness2,
+            "sandstone": sandstone,
+            "clay":clay,
+            "latitude": latitude,
+            "longitude": longitude,
+            "description": description
+        })
